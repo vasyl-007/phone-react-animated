e52aa2c5e83ab17982a7569aa7a6994d8fbf277f
Vasyl-007 <vasyl.manyo@gmail.com>
2020-04-06 19:03:33 +0300

    Updates

---
+++ precache-manifest.74d3a5dd0c3ec463bb869147941b1efa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aef0d244d2c40fcda5ae18d728a437f",
+    "url": "/phone-react-animated/index.html"
+  },
+  {
+    "revision": "23b6fbb0a17d755de989",
+    "url": "/phone-react-animated/static/css/main.ef37a4da.chunk.css"
+  },
+  {
+    "revision": "a809da6e04dc654187ca",
+    "url": "/phone-react-animated/static/js/2.709442de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/phone-react-animated/static/js/2.709442de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b6fbb0a17d755de989",
+    "url": "/phone-react-animated/static/js/main.8d589b3a.chunk.js"
+  },
+  {
+    "revision": "c1cec06430fb241885a6",
+    "url": "/phone-react-animated/static/js/runtime-main.7a9f10bf.js"
+  },
+  {
+    "revision": "f250dc652592cd94dafad7307c1e2723",
+    "url": "/phone-react-animated/static/media/phone-container-grey.f250dc65.jpg"
+  },
+  {
+    "revision": "3b2dbc4d68847bffecd1be245635eb2e",
+    "url": "/phone-react-animated/static/media/phone-container.3b2dbc4d.jpg"
+  }
+]);
